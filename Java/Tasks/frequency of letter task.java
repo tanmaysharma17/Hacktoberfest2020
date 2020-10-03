4b950022ec0975d7e732905de32f453d5f65f527
shwetaagrahari <sgraharishweta19@gmail.com>
2020-10-03 15:24:44 +0530

    TaskFrequencyOfLettersByShweta

--- Java/Tasks/frequency of letter task.java
+++ Java/Tasks/frequency of letter task.java
@@ -4,14 +4,11 @@ public class Main {
         String str = "this code is for counting the frequency of specific letter.";
         char ch = 'e';
         int frequency = 0;
-
-        // write your code here
-        //insert a loop which checks the number of e, and then print the final number of e in the sentence.
-
-        
-        
-        
-        
+		for(int i = 0; i < str.length(); i++) {
+            if(ch == str.charAt(i)) {
+                ++frequency;
+            }
+        }     
         System.out.println("Frequency of " + ch + " = " + frequency);
     }
 }
