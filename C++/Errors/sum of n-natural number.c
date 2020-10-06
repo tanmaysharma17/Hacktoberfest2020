f2dc3daacbe678ecc6683aec387e189c01fc1455
Piyush Pareek <59756782+PiyushPareek7221@users.noreply.github.com>
2020-10-06 11:58:01 +0530

    Error_solved_sum_of_n_natural_numbers

--- C++/Errors/sum of n-natural number.c
+++ C++/Errors/sum of n-natural number.c
@@ -1,14 +1,17 @@
-// The program will take the input from user and store it in variable 'number' ,
-// Then using a for loop it will find the sum of n natural numbers and prints it. 
-
+//Sum of N natural numbers
 #include<stdio.h>
-int main(){
-	int number, sum=0;
-	printf("Enter the number:- \n");
-	scanf("%d",&number);
-	for(int i=1;i<=number;i++){
-		sum=sum+i;
-	}
-	printf("The sum of %d natural numbers is equal to %d",number,sum);
-	return 0;
+int sum(int n){
+ if(n==0){
+   return 0;
+ }
+ return (n*(n+1))/2;
+}
+
+int main()
+{
+ int r;
+ scanf("%d",&r);
+ int result=sum(r);
+ printf("%d ",result);
+ return 0;
 }
