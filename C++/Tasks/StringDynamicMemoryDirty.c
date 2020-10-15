b83633d4af7c8843ac8bb3e81e27b6ec806bc4df
Shivam-999 <deeptijain9676@gmail.com>
2020-10-15 23:47:08 +0530

    StringDynamicMemory.c

--- C++/Tasks/StringDynamicMemoryDirty.c
+++ C++/Tasks/StringDynamicMemoryDirty.c
@@ -1,2 +1,24 @@
 //C program to read and print string, declaring memory of variables during run time.
 
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+	char *name;
+	int num;
+
+	printf("Enter max length of name: ");
+	scanf("%d",&num);
+
+		name=(char*)malloc(num*sizeof(char));
+
+	printf("Enter name: ");
+	getchar();
+	gets(name);
+
+	printf("Hi! %s, How are you?\n",name);
+
+	free(name);
+	getch();
+}
