d9d249c67c102c4116234c7b2671914696765cef
Sushmitha004 <sushmithahs004@gmail.com>
2020-10-28 23:45:30 +0530

    Resolved the error and updated the corrected file

--- C++/Errors/inserting in an array.c
+++ C++/Errors/inserting in an array.c
@@ -1,39 +1,41 @@
 #include<stdio.h>
 struct Array
 {
- int A[10];
- int size;
- int length;
+ 	int A[10];
+ 	int size;
+ 	int length;
 };
- void Display(struct Array arr)
- {
- int i;
- printf("\nElements are\n");
- for(i=0;i<arr.length;i++)
- printf("%d ",arr.A[i]);
- }
+
+void Display(struct Array arr)
+{
+ 	int i;
+ 	printf("\nElements are\n");
+ 	for(i = 0; i < arr.length; i++)
+ 		printf("%d ",arr.A[i]);
+}
 
  //error in the below segment
 //-------------
- void Insert(struct Array arr,int index,int x)
- {
- int i;
-
- if(index>=0 && index<=arr.length)
- {
- for(i=arr.length;i>index;i--)
- arr.A[i]=arr.A[i-1];
- arr.A[index]=x;
- arr.length--;
- }
- }
+void Insert(struct Array &arr,int index,int x)
+{
+	int i;
+  	if(index >= 0 && index <= arr.length)
+    {
+		for(i = arr.length; i >= index; i--)
+    	{
+      		arr.A[i] = arr.A[i-1];
+    	}
+ 		arr.A[index-1] = x;
+ 		arr.length++;
+    }
+}
 
 //-------------
 int main()
 {
-struct Array arr1={{2,3,4,5,6},10,5};
+	struct Array arr1 = {{2,3,4,5,6},10,5};
 
-Insert(arr1,4,172);
-Display(arr1);
- return 0;
+	Insert(arr1,4,172);
+	Display(arr1);
+ 	return 0;
 }
