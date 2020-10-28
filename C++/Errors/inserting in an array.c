750cc19501449a62725f2015a14fbc75f75abccc
Sushmitha004 <sushmithahs004@gmail.com>
2020-10-29 00:22:39 +0530

    Resolved the error and updated the corrected file

--- C++/Errors/inserting in an array.c
+++ C++/Errors/inserting in an array.c
@@ -21,11 +21,11 @@ void Insert(struct Array &arr,int index,int x)
 	int i;
   	if(index >= 0 && index <= arr.length)
     {
-		for(i = arr.length; i >= index; i--)
+		for(i = arr.length; i > index; i--)
     	{
       		arr.A[i] = arr.A[i-1];
     	}
- 		arr.A[index-1] = x;
+ 		arr.A[index] = x;
  		arr.length++;
     }
 }
